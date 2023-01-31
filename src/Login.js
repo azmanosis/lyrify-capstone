731f1949bfaa25f83bed701e96fd410c2555ec00
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-01-30 20:25:14 -0500

    Login Authorize page

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=b7a11319b71d4c9fb5ea10737cace61f&response_type=code&redirect_url=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+    return (
+        <div>
+
+        </div>
+    )
+}
