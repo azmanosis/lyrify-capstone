4d19bda9e1798bd355418bd0c4606d67c3318aa2
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-07 23:51:34 -0500

    player.js styling

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -16,6 +16,24 @@ function Player({ accessToken, trackUri }) {
             }}
             play={play}
             uris={trackUri ? [trackUri] : []}
+            styles={{
+                // activeColor: "#000",
+                // altColor: "#FFF",
+                bgColor: "#252e31", //Background color
+                color: "#aab182", // icon colors
+                // errorColor: "#FFF",
+                height: 100,
+                // loaderColor: "#FFF",
+                // loaderSize: 50,
+                // sliderColor: "#FFF",
+                // sliderHandleBorderRadius: 50,
+                // sliderHandleColor: "#FFF",
+                // sliderHeight: 50,
+                // sliderTrackBorderRadius: 50,
+                // sliderTrackColor: "#FFF",
+                // trackArtistColor: "#FFF",
+                // trackNameColor: "#FFF",
+            }}
         />
     )
 }
