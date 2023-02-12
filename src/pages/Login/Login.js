7d7ed5d1dad475e539b755a480c9a1140f0913d6
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-11 20:29:43 -0500

    dotenv module added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import './Login.scss';
 import React from 'react';
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=b7a11319b71d4c9fb5ea10737cace61f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=b7a11319b71d4c9fb5ea10737cace61f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 function Login() {
     return (
