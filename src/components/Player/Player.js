d40954a43f6db23a809464e9deb6513cf6fed002
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-11 20:29:59 -0500

    spotify player color changes

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -17,22 +17,22 @@ function Player({ accessToken, trackUri }) {
             play={play}
             uris={trackUri ? [trackUri] : []}
             styles={{
-                // activeColor: "#000",
+                // activeColor: "#252e31",
                 // altColor: "#FFF",
                 bgColor: "#252e31", //Background color
                 color: "#aab182", // icon colors
                 // errorColor: "#FFF",
-                height: 100,
+                height: 50, //album art made bigger
                 // loaderColor: "#FFF",
                 // loaderSize: 50,
-                // sliderColor: "#FFF",
+                // sliderColor: "#252e31",
                 // sliderHandleBorderRadius: 50,
                 // sliderHandleColor: "#FFF",
-                // sliderHeight: 50,
-                // sliderTrackBorderRadius: 50,
-                // sliderTrackColor: "#FFF",
-                // trackArtistColor: "#FFF",
-                // trackNameColor: "#FFF",
+                // sliderHeight: 50, //Slider height
+                sliderTrackBorderRadius: 50, //curve radius of slider
+                sliderTrackColor: "#FFF", //incomplete track color
+                trackArtistColor: "#FFF", //artist name color
+                trackNameColor: "#FFF", //song name color
             }}
         />
     )
