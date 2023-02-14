507260abbf306bcb50e93a1cb905301736336f62
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-14 00:37:17 -0500

    commented out windows / null

--- src/components/Auth/useAuth.js
+++ src/components/Auth/useAuth.js
@@ -15,7 +15,7 @@ function useAuth(code) {
             setAccessToken(res.data.accessToken)
             setRefreshToken(res.data.refreshToken)
             setExpiresIn(res.data.expiresIn)
-            window.history.pushState({}, null, '/dashboard')
+            // window.history.pushState({}, null, '/')
         })
         // .catch(() => {
         //     window.location = '/'
