ba8a8d5cc2dd206162d412cf1e18d31efc0bab81
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-17 01:57:26 -0500

    login layout changes

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import './Login.scss';
 import React from 'react';
+import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=b7a11319b71d4c9fb5ea10737cace61f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
@@ -7,8 +8,13 @@ function Login() {
     return (
         <>
             <div className="login">
-                <a className="login__url" href={AUTH_URL}>Login With Spotify</a>
-            </div >
+                <div className="login__header">
+                    <Logo />
+                </div>
+                <div className="login__spotify">
+                    <a className="login__spotify--url" href={AUTH_URL}>Login With Spotify</a>
+                </div>
+            </div>
         </>
     )
 }
