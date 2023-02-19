9a14251a0b60b3e2a34e839ef641c9e3cb7f9917
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-19 02:11:59 -0500

    Auth_URL update

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,7 @@ import './Login.scss';
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=b7a11319b71d4c9fb5ea10737cace61f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=b7a11319b71d4c9fb5ea10737cace61f&response_type=code&redirect_uri=http://localhost:3000/lyrify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 function Login() {
     return (
@@ -12,7 +12,7 @@ function Login() {
                     <Logo />
                 </div>
                 <div className="login__spotify">
-                    <a className="login__spotify--url" href={AUTH_URL}>Login With Spotify</a>
+                    <a className="login__spotify--url" href={AUTH_URL}>Login via Spotify</a>
                 </div>
             </div>
         </>
