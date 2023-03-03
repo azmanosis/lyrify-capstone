bbbfee3131638dc955af559909d6becd848338c3
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-03-03 12:54:42 -0500

    increase initialVolume to 50%

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -9,7 +9,7 @@ function Player({ accessToken, trackUri }) {
     if (!accessToken) return null
     return (
         <SpotifyPlayer
-            initialVolume={10}
+            initialVolume={50}
             token={accessToken}
             showSaveIcon={true}
             callback={state => {
